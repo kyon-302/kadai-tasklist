d8b1bee0cd28c5c92fdee6d505013da492667d8f
kotosinoeto <mirin_shouyu@outlook.jp>
2017-08-20 12:12:01 +0000

    add column　2

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,7 +4,7 @@
 
     <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
     
-    <p>タイトル: {{ $task->status }}</p>
+    <p>状況: {{ $task->status }}</p>
     <p>{{ $task->content }}</p>
     
     {!! link_to_route('tasks.edit', 'このタスクの編集', ['id' => $task->id]) !!}
